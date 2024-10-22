0417a1a1c84107a99e1eee61d459ae3dc152d833
Yavier Maldonado <yaviermaldonado@Yaviers-MacBook-Air.local>
2024-10-21 20:43:30 -0400

    Fixing betty on tsk 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
+ * main - Entry point
+ *
+ * Description: Prints a specific string using the puts function
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
