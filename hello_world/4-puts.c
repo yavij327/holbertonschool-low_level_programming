64aecabecb44d92e21b17278fdad5e7388c4dbf3
Yavier Maldonado <yaviermaldonado@Yaviers-MacBook-Air.local>
2024-10-21 20:26:41 -0400

    Uploading tsk 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * This code prints "Programming is like building a multilingual puzzle without using printf
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
