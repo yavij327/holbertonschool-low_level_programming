13b6c4c566ca3718aacdeeae3c25a74d462def4a
Yavier Maldonado <yaviermaldonado@Yaviers-MacBook-Air.local>
2024-10-21 20:32:04 -0400

    Fixing tsk 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 /**
- * This code prints "Programming is like building a multilingual puzzle without using printf
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
