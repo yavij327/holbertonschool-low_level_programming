423ec3ee8648896f87869d2c661b95eae610937e
Yavier Maldonado <yaviermaldonado@Yaviers-MacBook-Air.local>
2024-10-21 20:44:32 -0400

    Fixing betty on task 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: Prints a specific string using the puts function
+ * main - Entry point of the program
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
