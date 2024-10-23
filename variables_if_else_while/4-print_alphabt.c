5fc1c684baec1a38bdc199eab2b0dd1b4c02acc8
Yavier Maldonado <yaviermaldonado@Yaviers-MacBook-Air.local>
2024-10-23 13:27:41 -0400

    Uploading task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'e' || ch == 'q')
+		{
+		continue;
+		}
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
