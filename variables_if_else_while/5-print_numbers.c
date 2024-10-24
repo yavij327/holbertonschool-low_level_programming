817f28c7508b8e942d7821af82c6100dfdd87172
Yavier Maldonado <yaviermaldonado@Yaviers-MacBook-Air.local>
2024-10-23 21:13:56 -0400

    Uploading print all single digits from 0 to 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch < 10; ch++)
+	{
+		printf("%d", ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
