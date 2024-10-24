7c3050e89bbdd6e12c572c02c195951eb5bf8c52
Yavier Maldonado <yaviermaldonado@Yaviers-Air.localdomain>
2024-10-24 13:25:45 -0400

    Fixing betty on task 6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -2,11 +2,6 @@
 
 /**
  * main - Entry point of the program
- *
- * Description: This program prints all single-digit numbers of base 10
- * starting from 0, followed by a new line. It uses a loop and the
- * putchar function to achieve this.
- *
  * Return Always 0 (Success)
  */
 int main(void)
