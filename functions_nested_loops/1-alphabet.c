bb210b620a6a825fcf649a465c27fcfe3609c425
Yavier Maldonado <yaviermaldonado@Yaviers-MacBook-Air.local>
2024-10-27 14:57:45 -0400

    Fixing betty in task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * main - Entry point of the program
- *
- * Return: Always 0 (Success)
+ * print_alphabet - Prints the alphabet in lowercase
  */
-
 void print_alphabet(void)
 {
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
+	{
 		_putchar(ch);
-
+	}
 	_putchar('\n');
 }
 
