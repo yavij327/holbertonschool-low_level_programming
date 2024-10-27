87615e61d28dc91c93087d8e44fdc7e3edc2e743
Yavier Maldonado <yaviermaldonado@Yaviers-MacBook-Air.local>
2024-10-27 14:55:17 -0400

    Fixing betty in task 4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for lowercase characters
+ * _isalpha - Checks for alphabetic characters
+ * @c: The character to be checked
  *
- * Return: 1 if c is lowercase, 0 otherwise
+ * Return: 1 if c is a letter (lowercase or uppercase), 0 otherwise
  */
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-
+	{
 		return (1);
-
+	}
 	else
-
+	{
 		return (0);
+	}
 }
+
