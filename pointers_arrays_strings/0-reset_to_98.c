20fa4b494156c9914e694384e5c6d292ab8c250b
Yavier Maldonado <yaviermaldonado@Yaviers-Air.localdomain>
2024-10-30 11:42:46 -0400

    Uploading header file, README and task 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value of the integer it points to to 98
+ * @n: pointer to an integer
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Here we will start to learn about Pointers, Arrays and string in C
