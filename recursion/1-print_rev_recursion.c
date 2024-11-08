feb92820910c65bc9eff0e401be96b714961d8b9
Yavier Maldonado <yaviermaldonado@Yaviers-MacBook-Air.local>
2024-11-08 15:39:29 -0400

    Uploaded the file whitout compiling it

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - Prints a string in reverse, followed by a new line.
+ * _print_rev_recursion - Prints a string in reverse.
  * @s: The string to be reversed.
- *
- **/
+ */
 
 void _print_rev_recursion(char *s)
-
 {
 	if (*s == '\0')
-	return;
+		return;
+
 	_print_rev_recursion(s + 1);
 	_putchar(*s);
+}
