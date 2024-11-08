6226a0d55ac9e84e9522287dc9cadd3cff172656
Yavier Maldonado <yaviermaldonado@Yaviers-MacBook-Air.local>
2024-11-08 16:55:33 -0400

    Fixing betty on task 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -4,7 +4,6 @@
  * helper-Helps to find the square root by trying possible values recursively
  * @n: The number to find the square root of.
  * @guess: The current number being tested as the square root.
- *
  * Return: The natural square root of n, or -1 if none exists.
  */
 
