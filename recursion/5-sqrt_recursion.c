92b4d66255efc43dc1fb20369204fe523a005ed2
Yavier Maldonado <yaviermaldonado@Yaviers-MacBook-Air.local>
2024-11-08 16:53:23 -0400

    Uploading task 5 square root of a number

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * helper-Helps to find the square root by trying possible values recursively
+ * @n: The number to find the square root of.
+ * @guess: The current number being tested as the square root.
+ *
+ * Return: The natural square root of n, or -1 if none exists.
+ */
+
+int helper(int n, int guess)
+{
+	if (guess * guess == n)
+		return (guess);
+	if (guess * guess > n)
+		return (-1);
+	return (helper(n, guess + 1));
+}
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (helper(n, 1));
+}
+
