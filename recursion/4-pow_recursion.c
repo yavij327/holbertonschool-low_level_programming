d071d1a8044d4d2df07ef64ec172d0845a7a9c54
Yavier Maldonado <yaviermaldonado@Yaviers-MacBook-Air.local>
2024-11-08 16:25:31 -0400

    Fixing task 4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * _pow_recursion - Brief description of what the function does.
- * @x: Describe the parameter x.
- * @y: Describe the parameter y.
+ * _pow_recursion - Calculates x raised to the power of y.
+ * @x: The base number.
+ * @y: The exponent.
  *
- * Return: Description of what the function returns.
+ * Return: The result of x raised to the power of y, or -1 if y is negative.
  */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
 
-	if (x == 0)
+	if (y == 0)
 		return (1);
 
 	return (x * _pow_recursion(x, y - 1));
-
 }
