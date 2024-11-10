eb6e8a5313ab1033fd1bcc9f735c2b7a8afe5a5a
Yavier Maldonado <yaviermaldonado@Yaviers-MacBook-Air.local>
2024-11-10 13:25:36 -0400

    Fixing betty on task 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * helper - Checks if n is divisible by any number other than 1 and itself.
  * @n: The number to check.
@@ -16,6 +17,12 @@ int helper(int n, int divisor)
 	return (helper(n, divisor + 1));
 }
 
+/**
+ * is_prime_number - Determines if a number is prime.
+ * @n: The number to check.
+ *
+ * Return: 1 if n is a prime number, 0 otherwise.
+ */
 int is_prime_number(int n)
 {
 	if (n < 2)
