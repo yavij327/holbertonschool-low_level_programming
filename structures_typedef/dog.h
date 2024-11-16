1150da60129bb11054f36eb638629dfc60996d78
Yavier Maldonado <yaviermaldonado@Yaviers-MacBook-Air.local>
2024-11-16 14:43:13 -0400

    Uploading task 3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,6 +16,9 @@ struct dog
 	char *owner;
 };
 
+/* Define dog_t as a new name for struct dog */
+typedef struct dog dog_t;
+
 /* Function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
