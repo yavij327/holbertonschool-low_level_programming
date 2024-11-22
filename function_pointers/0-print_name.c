f113073a18bb791b0ff9c247d329477055a98b20
Yavier Maldonado <yaviermaldonado@Yaviers-MacBook-Air.local>
2024-11-22 13:58:52 -0400

    Uploading header file and task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - Prints a name using a function pointer.
+ * @name: The name to print.
+ * @f: A pointer to the function to use for printing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+
+{
+	if (name == NULL || f ==  NULL)
+		return;
+
+		f(name);
+}
+
