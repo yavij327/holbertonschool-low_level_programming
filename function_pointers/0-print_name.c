88627f41730d3cc58f68793f8fce120b1d4f3d5e
Yavier Maldonado <yaviermaldonado@Yaviers-MacBook-Air.local>
2024-11-22 14:34:46 -0400

    Fixing task 0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -5,14 +5,13 @@
  * print_name - Prints a name using a function pointer.
  * @name: The name to print.
  * @f: A pointer to the function to use for printing.
+ *
+ * Return: void
  */
-
 void print_name(char *name, void (*f)(char *))
-
 {
-	if (name == NULL || f ==  NULL)
-		return;
+    if (name == NULL || f == NULL)
+        return;
 
-		f(name);
-
-
+    f(name);
+}
