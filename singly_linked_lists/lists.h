9321b8b6949c4d7cdeadfbade766278ad26a6417
Yavier Maldonado <yaviermaldonado@Yaviers-MacBook-Air.local>
2024-12-05 21:48:37 -0400

    Uploading garbage files to delete them later

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,7 +1,25 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
 
-int _putchar(char c)
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct list_s - Singly linked list
+ * @str: String (malloc'ed string)
+ * @len: Length of the string
+ * @next: Points to the next node
+ */
+
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
